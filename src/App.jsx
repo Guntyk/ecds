3fc0c54a1e7e0f4c2dd6af648a6abe25a1635a29
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-07-05 10:50:42 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Switch, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <main>
+      <Switch>
+        <Route path='/' exact>
+          Hello world
+        </Route>
+        <Route>
+          Not found
+        </Route>
+      </Switch>
+    </main>
+  );
+}
