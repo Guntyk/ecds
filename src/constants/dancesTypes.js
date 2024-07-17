0e388ac0275236ce97a56665635f49dbabcf1b03
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-07-17 08:09:35 +0300

    add constants

---
+++ src/constants/dancesTypes.js
@@ -0,0 +1,8 @@
+import ballroom from 'assets/images/ballroom-couple.jpg';
+
+export const dancesTypes = [
+  { id: 1, name: 'Ballroom dance', image: ballroom },
+  { id: 2, name: 'Street dance' },
+  { id: 3, name: 'Caribbean dance' },
+  { id: 4, name: 'Other dances' },
+];
---
+++ src/constants/principlesData.js
@@ -0,0 +1,30 @@
+export const principlesData = [
+  {
+    id: 1,
+    title: 'Sport',
+    text: 'Open Tournaments but Closed Championships. National teams, ratings, titles.',
+    conclusion: 'The laws of sports are above all!',
+    iconsColor: 'pink',
+  },
+  {
+    id: 2,
+    title: 'Exclusivity',
+    text: 'One organization per country. Certification of clubs, coaches, judges.',
+    conclusion: 'Membership in the ECDS guarantees the quality of work.',
+    iconsColor: 'blue',
+  },
+  {
+    id: 3,
+    title: 'Standardization',
+    text: 'Single database, rules, requirements for tournaments, judges, dancers of all levels.',
+    conclusion: 'Dance Europe is understandable and accessible.',
+    iconsColor: 'lavender',
+  },
+  {
+    id: 4,
+    title: 'Win-win strategy',
+    text: 'Comfort and respect. Long-term cooperation. Positive outcome for all parties.',
+    conclusion: 'Everyone wins with us.',
+    iconsColor: 'green',
+  },
+];
