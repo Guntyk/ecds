5af7c78f28f5970d809e4246cc346a379eca4d81
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-07-18 20:06:57 +0300

    create hero block

---
+++ src/pages/Main/blocks/Hero/Hero.jsx
@@ -0,0 +1,69 @@
+import { EffectFade, Pagination, Navigation, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { mockedAds } from 'constants/mockedAds';
+import { Container } from 'components/Container';
+import { Button } from 'components/Button';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css';
+import styles from 'pages/Main/blocks/Hero/Hero.scss';
+
+SwiperCore.use([EffectFade, Navigation, Pagination, Autoplay]);
+
+export const Hero = () => {
+  const pagination = {
+    bulletActiveClass: styles.bulletActive,
+    bulletClass: styles.bullet,
+    el: '#pagination',
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '</span>';
+    },
+  };
+
+  return (
+    <section className={styles.block}>
+      <Container>
+        <div className={styles.wrapper}>
+          <div className={styles.titleWrapper}>
+            <h1 className={styles.title}>Unified Standards for a United Europe</h1>
+            <Button buttonContent='Registration' />
+          </div>
+          <div className={styles.sliderWrapper}>
+            <Swiper
+              loop={true}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: true,
+              }}
+              effect={'fade'}
+              pagination={pagination}
+              navigation={{ nextEl: '#next', prevEl: '#prev' }}
+            >
+              {mockedAds.map((Component) => (
+                <SwiperSlide>
+                  <Component />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <div className={styles.navigation}>
+              <Arrow id='prev' />
+              <div id='pagination' className={styles.pagination}></div>
+              <Arrow id='next' />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+const Arrow = ({ id }) => {
+  return (
+    <div id={id} className={styles.arrow}>
+      <span />
+      <span />
+    </div>
+  );
+};
