db5fcbdfb0cb8c191256a53f586bc144006eaba1
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-07-22 11:47:26 +0300

    create career block

---
+++ src/pages/Main/blocks/Career/Career.jsx
@@ -0,0 +1,11 @@
+import { Container } from 'components/Container';
+import styles from 'pages/Main/blocks/Career/Career.scss';
+
+export const Career = () => (
+  <section className={styles.block}>
+    <Container>
+      <h2 className={styles.title}>Game all the way</h2>
+      <p className={styles.subtitle}>Scheme of career growth according to our standards</p>
+    </Container>
+  </section>
+);
