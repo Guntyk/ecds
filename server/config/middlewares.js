846bf7804d466154a40948e3594b4a84b408c979
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-08-18 14:15:38 +0300

    modify middlewares

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,45 +1,12 @@
-module.exports = {
-  settings: {
-    logger: {
-      level: "info",
-    },
-    errors: {
-      stack: false,
-    },
-    security: {
-      headers: {
-        contentSecurityPolicy: false,
-      },
-    },
-    cors: {
-      enabled: true,
-      origin: "http://185.25.117.139/",
-    },
-    poweredBy: {
-      enabled: false,
-    },
-    query: {
-      limit: 10000,
-    },
-    body: {
-      multiparty: true,
-    },
-    session: {
-      enabled: true,
-      name: "strapi.sid",
-      secret: process.env.APP_KEYS,
-      rolling: true,
-      cookie: {
-        maxAge: 86400000,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-      },
-    },
-    favicon: {
-      path: "./public/favicon.ico",
-    },
-    public: {
-      path: "./public",
-    },
-  },
-};
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
