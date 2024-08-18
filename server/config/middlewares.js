1022444b6db7e53f3b8d85d8e3e50fc74d32d741
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-08-18 16:50:03 +0300

    update server content security policy

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,13 +1,47 @@
 module.exports = [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "default-src": ["'self'", "https:"],
+          "script-src": ["'self'", "https:"],
+          "style-src": ["'self'", "https:", "'unsafe-inline'"],
+          "img-src": ["'self'", "data:", "blob:", "https://euro-dance.org"],
+          "connect-src": [
+            "'self'",
+            "https:",
+            "http://localhost:1337",
+            "ws:",
+            "wss:",
+            "http://euro-dance.org",
+            "https://euro-dance.org",
+          ],
+          "frame-ancestors": [
+            "'self'",
+            "http://euro-dance.org",
+            "https://euro-dance.org",
+          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          "object-src": ["'none'"],
+          "upgrade-insecure-requests": [],
+        },
+      },
+    },
+  },
   {
     name: "strapi::cors",
     config: {
       enabled: true,
-      origin: ["http://localhost:1337", "http://euro-dance.org"],
-      methods: ["GET", "POST", "PUT"],
+      origin: [
+        "http://localhost:1337",
+        "http://euro-dance.org",
+        "https://euro-dance.org",
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
       keepHeaderOnError: true,
       credentials: true,
