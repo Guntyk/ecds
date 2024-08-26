41e469329f34a7acf667bd6b68f5cb810475e4a3
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-08-26 09:14:30 +0300

    update news info page design

--- client/src/pages/News/NewsInfo/NewsInfo.jsx
+++ client/src/pages/News/NewsInfo/NewsInfo.jsx
@@ -1,17 +1,27 @@
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { getNews, updateViews } from '@redux/features/newsSlice';
 import { formatDate } from 'helpers/formatDate';
 import { ImageComponent } from 'components/Image';
 import { pathnames } from 'constants/pathnames';
 import { Container } from 'components/Container';
+import { Button } from 'components/Button';
 import { Loader } from 'components/Loader';
 import { Link } from 'components/Link';
 import { NotFound } from 'pages/Services/NotFound';
 import { TryAgain } from 'pages/Services/TryAgain';
+import shareIcon from 'assets/icons/share.svg';
 import styles from 'pages/News/NewsInfo/NewsInfo.scss';
+
+const useArticles = (news, newsId) => {
+  return useMemo(() => {
+    const currentArticle = news.find((article) => article.id === newsId);
+    const nextArticle = news[news.findIndex((article) => article.id === newsId) + 1];
+    return { currentArticle, nextArticle };
+  }, [news, newsId]);
+};
 
 export const NewsInfo = () => {
   const { isLoading, error, news } = useSelector((state) => state.news);
@@ -20,8 +30,7 @@ export const NewsInfo = () => {
   const { id } = useParams();
   const newsId = Number(id);
 
-  const currentArticle = news.find((article) => article.id === newsId);
-  const nextArticle = news[news.findIndex((article) => article.id === newsId) + 1];
+  const { currentArticle, nextArticle } = useArticles(news, newsId);
 
   useEffect(() => {
     if (!news.length) {
@@ -76,9 +85,23 @@ export const NewsInfo = () => {
             </section>
           )}
           <section>
-            <p className={styles.date}>{formatDate(publicationDate)}</p>
             <h1 className={styles.title}>{title}</h1>
             {description && <h2 className={styles.description}>{description}</h2>}
+            <div className={styles.credits}>
+              <time dateTime={publicationDate} className={styles.date}>
+                {formatDate(publicationDate)}
+              </time>
+            </div>
+            <hr className={styles.line} />
+            <div className={styles.promotion}>
+              <Button className={styles.shareBtn} ghostStyle>
+                Share <img src={shareIcon} alt='Share article' />
+              </Button>
+              <p className={styles.views}>
+                <span className={styles.viewsIcon} />
+                {views}
+              </p>
+            </div>
             <div className={styles.content}>
               <BlocksRenderer content={content} />
             </div>
