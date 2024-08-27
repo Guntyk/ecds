dca7926ca22decb193dcc56cbc819192f328e437
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-08-27 16:04:01 +0300

    adapt header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import { Link, NavLink, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import cn from 'classnames';
+import useWindowDimensions from 'hooks/useWindowDimensions';
 import { pathnames } from 'constants/pathnames';
 import { Container } from 'components/Container';
 import { Button } from 'components/Button';
 import { Menu } from 'components/Menu';
+import logoSmall from 'assets/icons/logos/ecds-compressed.svg';
 import logo from 'assets/icons/logos/ecds.svg';
+import menu from 'assets/icons/menu.svg';
 import styles from 'components/Header/Header.scss';
 
 export const Header = () => {
@@ -41,34 +44,45 @@ export const Header = () => {
     }
   };
 
+  const { width } = useWindowDimensions();
+
   const currentPathName = '/' + pathname.split('/')[1];
 
   return (
-    <Container>
-      <header className={cn(styles.header, { [styles.headerDark]: darkHeaderPages.includes(currentPathName) })}>
-        <Link to={mainPage} className={styles.logo}>
-          <img src={logo} alt='European Confederation of Dance Sports logo' />
-          European Confederation of Dance Sports
-        </Link>
-        <nav>
-          <ul className={styles.navigation}>
-            <li>
-              <NavLink
-                className={cn(styles.navigationLink, { [styles.active]: aboutUsPages.includes(currentPathName) })}
-                to={managementPage}
-                onClick={handleAboutUsLinkClick}
-                {...(aboutUsPages.includes(currentPathName) && { tabIndex: -1 })}
-              >
-                About us
-              </NavLink>
-            </li>
-            <li>
-              <Button className={styles.navigationLink} text='Menu' onClick={() => setIsMenuOpen(true)} noStyle />
-            </li>
-          </ul>
-        </nav>
-        <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
-      </header>
-    </Container>
+    <header className={styles.headerWrapper}>
+      <Container>
+        <div className={cn(styles.header, { [styles.headerDark]: darkHeaderPages.includes(currentPathName) })}>
+          <Link to={mainPage} className={styles.logo}>
+            <img src={width > 557 ? logo : logoSmall} alt='European Confederation of Dance Sports logo' />
+            {width > 557 && 'European Confederation of Dance Sports'}
+          </Link>
+          <nav>
+            <ul className={styles.navigation}>
+              {width > 557 && (
+                <li>
+                  <NavLink
+                    className={cn(styles.navigationLink, { [styles.active]: aboutUsPages.includes(currentPathName) })}
+                    to={managementPage}
+                    onClick={handleAboutUsLinkClick}
+                    {...(aboutUsPages.includes(currentPathName) && { tabIndex: -1 })}
+                  >
+                    About us
+                  </NavLink>
+                </li>
+              )}
+              <li>
+                <Button
+                  className={styles.navigationLink}
+                  text={width > 557 ? 'Menu' : <img src={menu} alt='Menu' />}
+                  onClick={() => setIsMenuOpen(true)}
+                  noStyle
+                />
+              </li>
+            </ul>
+          </nav>
+          <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
+        </div>
+      </Container>
+    </header>
   );
 };
