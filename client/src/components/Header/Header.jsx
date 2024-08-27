70a0dc342f9cade83e631e7f40586d0c0c7a9482
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-08-27 17:31:59 +0300

    small menu adaptation fixes

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -49,43 +49,46 @@ export const Header = () => {
   const isPageDark = darkHeaderPages.includes(currentPathName);
 
   return (
-    <header className={styles.headerWrapper}>
-      <Container>
-        <div className={cn(styles.header, { [styles.headerDark]: isPageDark })}>
-          <Link to={mainPage} className={styles.logo}>
-            <img
-              src={screenWidth > 557 ? logo : isPageDark ? logoSmallDark : logoSmall}
-              alt='European Confederation of Dance Sports logo'
-            />
-            {screenWidth > 557 && 'European Confederation of Dance Sports'}
-          </Link>
-          <nav>
-            <ul className={styles.navigation}>
-              {screenWidth > 557 && (
+    <>
+      <header className={styles.headerWrapper}>
+        <Container>
+          <div className={cn(styles.header, { [styles.headerDark]: isPageDark })}>
+            <Link to={mainPage} className={styles.logo}>
+              <img
+                src={screenWidth > 557 ? logo : isPageDark ? logoSmallDark : logoSmall}
+                alt='European Confederation of Dance Sports logo'
+              />
+              {screenWidth > 557 && 'European Confederation of Dance Sports'}
+            </Link>
+            <nav>
+              <ul className={styles.navigation}>
+                {screenWidth > 557 && (
+                  <li>
+                    <NavLink
+                      className={cn(styles.navigationLink, { [styles.active]: aboutUsPages.includes(currentPathName) })}
+                      to={managementPage}
+                      onClick={handleAboutUsLinkClick}
+                      {...(aboutUsPages.includes(currentPathName) && { tabIndex: -1 })}
+                    >
+                      About us
+                    </NavLink>
+                  </li>
+                )}
                 <li>
-                  <NavLink
-                    className={cn(styles.navigationLink, { [styles.active]: aboutUsPages.includes(currentPathName) })}
-                    to={managementPage}
-                    onClick={handleAboutUsLinkClick}
-                    {...(aboutUsPages.includes(currentPathName) && { tabIndex: -1 })}
-                  >
-                    About us
-                  </NavLink>
+                  <Button
+                    className={styles.navigationLink}
+                    text={screenWidth > 557 ? 'Menu' : <div className={styles.menuIcon} />}
+                    onClick={() => setIsMenuOpen(true)}
+                    noStyle
+                  />
                 </li>
-              )}
-              <li>
-                <Button
-                  className={styles.navigationLink}
-                  text={screenWidth > 557 ? 'Menu' : <div className={styles.menuIcon} />}
-                  onClick={() => setIsMenuOpen(true)}
-                  noStyle
-                />
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
-      </Container>
-    </header>
+              </ul>
+            </nav>
+          </div>
+          {screenWidth > 557 && <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />}
+        </Container>
+      </header>
+      {screenWidth <= 557 && <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />}
+    </>
   );
 };
