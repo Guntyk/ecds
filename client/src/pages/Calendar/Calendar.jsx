6fc82142c8c320c84d7c931d4fbfc9f14a64670a
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-08-31 09:53:45 +0300

    add functionality for dividing events into dance styles

--- client/src/pages/Calendar/Calendar.jsx
+++ client/src/pages/Calendar/Calendar.jsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { getEvents } from '@redux/features/eventsSlice';
 import { Container } from 'components/Container';
@@ -6,16 +7,29 @@ import { Loader } from 'components/Loader';
 import { EventCard } from 'pages/Calendar/EventCard';
 import { TryAgain } from 'pages/Services/TryAgain';
 import styles from 'pages/Calendar/Calendar.scss';
+import { pathnames } from 'constants/pathnames';
 
 export const Calendar = () => {
   const { isLoading, error, events } = useSelector((state) => state.events);
+  const { ballroomPage, streetDancePage, caribbeanDancePage } = pathnames;
+  const { pathname } = useLocation();
   const dispatch = useDispatch();
 
+  let danceStyle = '';
+
+  if (pathname.includes(ballroomPage)) {
+    danceStyle = 'Ballroom';
+  } else if (pathname.includes(streetDancePage)) {
+    danceStyle = 'Street';
+  } else if (pathname.includes(caribbeanDancePage)) {
+    danceStyle = 'Caribbean';
+  }
+
   useEffect(() => {
-    if (!events.length) {
-      dispatch(getEvents());
+    if (danceStyle && !events.length) {
+      dispatch(getEvents({ danceStyle }));
     }
-  }, []);
+  }, [danceStyle]);
 
   if (error) {
     return <TryAgain />;
